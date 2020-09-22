fcf01b4ee3e721314a70577cf2ee3e6ce55d9605
Alaercio Vieira <alaercio.vieira@outlook.com>
2020-09-21 22:26:09 -0300

    Updates

---
+++ precache-manifest.3d8fcc750235ee6c61659dbad4076f48.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4a066015d6713145b07c393fcca9ac1",
+    "url": "/CloneNetfilx--React/index.html"
+  },
+  {
+    "revision": "652e4b5add3e0133737b",
+    "url": "/CloneNetfilx--React/static/css/main.e33d8561.chunk.css"
+  },
+  {
+    "revision": "10438ded368ecc6ad310",
+    "url": "/CloneNetfilx--React/static/js/2.3b2dc6e2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/CloneNetfilx--React/static/js/2.3b2dc6e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "652e4b5add3e0133737b",
+    "url": "/CloneNetfilx--React/static/js/main.c2495870.chunk.js"
+  },
+  {
+    "revision": "4cdd2c8bc5f350edb096",
+    "url": "/CloneNetfilx--React/static/js/runtime-main.5c87d37f.js"
+  },
+  {
+    "revision": "7bcb00951048e6114939f4a0c366c79a",
+    "url": "/CloneNetfilx--React/static/media/loading.7bcb0095.gif"
+  },
+  {
+    "revision": "6933981bf7675ce241c63331f1832241",
+    "url": "/CloneNetfilx--React/static/media/logo.6933981b.png"
+  }
+]);
